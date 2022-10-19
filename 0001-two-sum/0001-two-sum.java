42aa063475cda50c5a6a03d0ad6171dd1b953965
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2022-10-19 15:33:18 +0200

    Time: 66 ms (39.37%), Space: 42.1 MB (98.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,36 +3,19 @@ class Solution {
         
         int [] myIndices = new int[2];
         
-       // int counter = 0;
         
         for(int i = 0;i<nums.length -1 ;i++){
             
-          // int counter = i; 
             for(int j = i+1;j < nums.length ;j++){
                 
                 
-                /* if(nums[i] == target){
-              
-                myIndices[0] = i;
-                   //  break;
-                //return myIndices;
-               }*/
-                
-               /* else if( (nums[i]+nums[i+1]) == target   ){
-                
-                myIndices[0] = i;
-                myIndices[1] = counter++;
-                    break;
-               // return myIndices;
-            }*/
                  if((nums[i] + nums[j]) == target){
                     
                   myIndices[0] = i;
                   myIndices[1] = j;  
-                   // break;
+                  break;
                 }
-                
-             //counter = 0;   
+               
             }
         }
         
