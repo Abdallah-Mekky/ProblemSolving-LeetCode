75e60d3ad08e295f828563e515dada7b2f970844
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2022-10-19 20:53:17 +0200

    Time: 12 ms (5.88%), Space: 50.3 MB (28.11%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+      int [] answer = new int[(2*nums.length)];
+        
+       // answer = Arrays.copyOf(nums,nums.length);
+        
+       // int size = answer.length/2;
+        int counter = 0 ;
+        
+        for(int i = 0 ;i<answer.length;i++){
+            
+            
+            
+            if( counter == nums.length  ){
+                
+                counter = 0;
+            }
+            
+            answer[i] = nums[counter];
+            counter++;
+        }
+        
+        
+       
+        
+        return answer;
+    }
+}
