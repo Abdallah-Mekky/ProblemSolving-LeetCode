f5f05b20c3a5a5df5e6035ae081d6064da3c618f
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2022-10-20 22:57:03 +0200

    Time: 105 ms (5.02%), Space: 49.4 MB (72.09%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -1,37 +1,26 @@
 class Solution {
     public int findMaxK(int[] nums) {
         
-        int maxNumber = nums[0];
-      //  ArrayList<Integer> myMaxNumbers = new ArrayList<Integer>();
-       // int finalMax = 0;
-        
-        for(int i =0;i<nums.length;i++){
-            
-            if(nums[i] != -maxNumber){
-                maxNumber = -1;
-            }
-        }
+        int maxNumber = 0;
+        ArrayList<Integer> myMaxNumbers = new ArrayList<Integer>();
+        int finalMax = 0;
       
         
         for(int i =0;i<nums.length;i++){
             
             for(int j=0;j<nums.length;j++){
                 
-                if( nums[j] == (-nums[i])){
-                    
-                   if(nums[j] > maxNumber ){
+                if( nums[j] == (-nums[i]) && nums[j] > maxNumber ){
                     
                     maxNumber = nums[j];
-                   // myMaxNumbers.add(maxNumber);
+                    myMaxNumbers.add(maxNumber);
                     
-                }  
                 }
-                   
             }
             
         }
          
-       /* for(int i=0;i<myMaxNumbers.size();i++){
+        for(int i=0;i<myMaxNumbers.size();i++){
             
             if(myMaxNumbers.get(i) > finalMax ){
                 
@@ -46,9 +35,7 @@ class Solution {
         
         
         
-       return -1;*/
-        
-        return maxNumber;
+       return -1;
         
         
     }
