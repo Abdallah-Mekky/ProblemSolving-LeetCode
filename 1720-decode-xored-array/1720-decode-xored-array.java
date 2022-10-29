e1c678bbdf316d0e310823fac4fc0dfae183d963
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2022-10-29 18:14:43 +0200

    Time: 2 ms (83.44%), Space: 56.4 MB (33.96%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -7,7 +7,7 @@ class Solution {
         
         for(int i =0;i<encoded.length;i++){
            
-            decodedArray[i+1] =  decodedArray[i] ^ encoded[i] ;
+            decodedArray[i+1] =  encoded[i] ^ decodedArray[i];
         }
         
         return decodedArray;
