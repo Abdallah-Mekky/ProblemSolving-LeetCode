0bb619b9e84df332a7701d28672d0e595b009b46
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2022-11-02 16:41:29 +0200

    Time: 1300 ms (13.16%), Space: 47.5 MB (22.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        String reversedWord;
+       String reversedWord;
         StringBuilder s1;
         
         if(s == null){
             return false;
         }
         
-        if(s.length() == 0){
+        else{
+          
+            if(s.length() == 0){
             
-            return true;}
-        
-         
-             s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+            return true;
+        }else{
+            
+            s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
              s1 = new StringBuilder(s);
-             reversedWord = s1.reverse().toString();   
-       
+            reversedWord = s1.reverse().toString();   
+        }   
+        }
+        
+        
         
         return s.equals(reversedWord);
+        
         
     }
 }
