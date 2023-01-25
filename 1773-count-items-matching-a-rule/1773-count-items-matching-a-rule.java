18d6b5fe53aa474dd31b7992334a34cd7f57c6c5
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-01-25 17:55:59 +0200

    Time: 6 ms (46.04%), Space: 57.1 MB (10.93%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -6,52 +6,33 @@ class Solution {
         if(ruleKey.equals("type")){
                 
             for(int i = 0 ;i<items.size();i++){
+                
                 if(items.get(i).get(0).equals(ruleValue)){
                     
                     counter++;
-                }}
+                }
+            }
         
         }else if(ruleKey.equals("color")){
                 
                  for(int i = 0 ;i<items.size();i++){
+                     
                 if(items.get(i).get(1).equals(ruleValue)){
                     
                     counter++;
-                }}
+                }
+                 }
+            
         }else if(ruleKey.equals("name")){
                 
                 for(int i = 0 ;i<items.size();i++){
+                    
                 if(items.get(i).get(2).equals(ruleValue)){
                     
                     counter++;
-                }}
+                }
+                }
             }
-        
-        
-//         for(int i = 0 ;i<items.size();i++){
-            
-//             if(ruleKey.equals("type")){
-                
-//                 if(items.get(i).get(0).equals(ruleValue)){
-                    
-//                     counter++;
-//                 }
-                
-//             }else if(ruleKey.equals("color")){
-                
-//                 if(items.get(i).get(1).equals(ruleValue)){
-                    
-//                     counter++;
-//                 }
-                
-//             }else if(ruleKey.equals("name")){
-                
-//                 if(items.get(i).get(2) == ruleValue){
-                    
-//                     counter++;
-//                 }
-//             }
-//         }
         
         return counter;
     }
