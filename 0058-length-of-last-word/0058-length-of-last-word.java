12f734a629283945902b62c2a5149d2ebb7f4c8b
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-01-30 17:57:25 +0200

    Time: 0 ms (100.00%), Space: 40.1 MB (98.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -12,26 +12,20 @@ class Solution {
         
         
         for(int i = s.length()-1;i>0;i--){
-            
-            
-            
+              
             if(s.charAt(i) != ' '){
              sizeOfLastWord++;
+                
+                if(s.charAt(i - 1) == ' ' ){
+                
+                       break;   
+                }
              
-             if(i == 1 && s.charAt(0) != ' '){
+                else if(i == 1 && s.charAt(0) != ' '){
                    
                        sizeOfLastWord++;
-                   }else if(s.charAt(i - 1) == ' ' ){
-                
-                   //else{
-                   
-                       break;
-                   //}
-                    
-                }
             }
-             
-            
+            }
         }
         
         return sizeOfLastWord;
