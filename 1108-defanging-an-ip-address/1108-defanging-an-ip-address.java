3c77e903dbb18f479d4aaa35d60eac9f2df42fc3
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-01-30 21:04:58 +0200

    Time: 0 ms (100.00%), Space: 40.4 MB (64.97%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -16,8 +16,13 @@ class Solution {
         
         StringBuilder s =new StringBuilder();
         for(int i = 0 ; i <address.length() ;i++ ){
-            if(address.charAt(i)=='.') s.append("[.]");
-            else s.append(address.charAt(i));
+            if(address.charAt(i)=='.') 
+            {
+                s.append("[.]");
+            }
+            else{
+                s.append(address.charAt(i));
+            }
         }
         return String.valueOf(s);
         
