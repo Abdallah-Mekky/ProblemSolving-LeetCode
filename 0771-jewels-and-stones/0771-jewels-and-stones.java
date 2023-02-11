8c20e4affe2ca57b715de62236241587a6af19eb
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-02-11 16:11:13 +0200

    Time: 0 ms (100.00%), Space: 40.6 MB (75.93%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,20 +3,27 @@ class Solution {
         
         int sumOfJewelsInStones = 0;
         
-        for(int i = 0;i<jewels.length();i++){
+//         for(int i = 0;i<jewels.length();i++){
             
-            char currentJewel = jewels.charAt(i);
+//             char currentJewel = jewels.charAt(i);
             
-            for(int j = 0;j<stones.length();j++){
+//             for(int j = 0;j<stones.length();j++){
                 
-                 char currentStone = stones.charAt(j);
+//                  char currentStone = stones.charAt(j);
                 
-                if(currentJewel == currentStone){
+//                 if(currentJewel == currentStone){
                     
-                    sumOfJewelsInStones++;
+//                     sumOfJewelsInStones++;
                     
-                }
-            }
+//                 }
+//             }
+//         }
+        
+        for(int i = 0;i<stones.length();i++){
+            
+            if(jewels.indexOf(stones.charAt(i)) != -1){
+              sumOfJewelsInStones++;  
+            }      
         }
         
         return sumOfJewelsInStones;
