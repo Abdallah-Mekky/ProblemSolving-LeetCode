2b152ba65c42f5bf0897619a08d73af787cdf0c5
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-02-11 17:30:28 +0200

    Time: 1 ms (77.40%), Space: 40.5 MB (50.58%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String sortSentence(String s) {
+     
+        
+        //divide the sentence into words with delimiter space
+        String [] wordsOfSentence = s.split(" ");
+        
+        //contains the words in order
+        String [] orderedSentence = new String[wordsOfSentence.length];
+        
+        //real index of each word in orderedSentence array
+        int numOfWord = 0;
+        
+        for(int i = 0; i<wordsOfSentence.length; i++){
+            
+    //note : each word that represent string
+    //get the index from each word 
+    numOfWord = 
+        Integer.valueOf(wordsOfSentence[i].substring(wordsOfSentence[i].length() - 1));
+                
+    orderedSentence[numOfWord - 1] =
+        wordsOfSentence[i].substring(0,wordsOfSentence[i].length() - 1);    
+        }
+        
+        return String.join(" ",orderedSentence);
+    }
+}
