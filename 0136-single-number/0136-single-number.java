6ffd3cce00cdd6d8ed3b33bdc1cb8dab8ce7b27d
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-02-12 23:02:05 +0200

    Time: 1 ms (100.00%), Space: 42.5 MB (56.29%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       
+        int singleNum = 0;
+        
+        for(int i = 0;i<nums.length;i++){
+            
+            singleNum ^= nums[i];
+        }
+       
+       return singleNum;
+    }
+}
