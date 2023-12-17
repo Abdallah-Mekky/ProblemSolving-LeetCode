d4d8efce4728a1b85f1ccb40f880e359de9ccbe1
Abdallah Mohamed Mekky <80880411+Abdallah-Mekky@users.noreply.github.com>
2023-12-17 22:53:30 +0200

    Time: 39 ms (42.94%), Space: 44 MB (17.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        for (int i = 0;i<nums.length;i++){
+        for (int i =0;i<nums.length;i++){
             
-            for (int j = i+1;j<=nums.length-1;j++){
+            for (int j = nums.length -1 ;j>i;j--){
                 
                 int temp = nums[i] + nums[j];
-                    
+                
                     if (temp == target){
                         return new int[]{i,j};
                     }
             }
             
         }
+        
+        
+        
+//         for (int i = 0;i<nums.length;i++){
+            
+//             for (int j = i+1;j<=nums.length-1;j++){
+                
+//                 int temp = nums[i] + nums[j];
+                    
+//                     if (temp == target){
+//                         return new int[]{i,j};
+//                     }
+//             }
+            
+//         }
         
         return null;
     }
